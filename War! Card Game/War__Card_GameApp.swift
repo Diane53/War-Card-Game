3c998f71e23d4c050378bb8b8c7f2693bf048d17
Diane R. Warde <dianewarde@icloud.com>
2021-05-30 14:25:32 -0400

    Initial Commit

---
+++ War! Card Game/War__Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War__Card_GameApp.swift
+//  War! Card Game
+//
+//  Created by Xcode Server on 5/30/21.
+//
+
+import SwiftUI
+
+@main
+struct War__Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ War! Card GameTests/War__Card_GameTests.swift
@@ -0,0 +1,33 @@
+//
+//  War__Card_GameTests.swift
+//  War! Card GameTests
+//
+//  Created by Xcode Server on 5/30/21.
+//
+
+import XCTest
+@testable import War__Card_Game
+
+class War__Card_GameTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
