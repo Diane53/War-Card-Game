e0e1d826a1517cab24fa2f9517849e5b076ed4a3
Diane R. Warde <dianewarde@icloud.com>
2021-06-03 18:58:18 -0400

    add 33 image assets

---
+++ War! Card Game/Card Model/CardModel.swift
@@ -0,0 +1,12 @@
+//
+//  CardModel.swift
+//  War! Card Game
+//
+//  Created by Xcode Server on 6/3/21.
+//
+
+import Foundation
+
+var playerStartDeck = ["card 1", "card 2", "card 5", "card 6", "card 9", "card 10", "card 13":5, "card 14":5, "card 17":6, "card 18":6, "card 21":7, "card 22":7, "card 25":8, "card 26":8, "card 29":9, "card 30":9, "card 33":10, "card 34":10, "card 37":11, "card 39":11, "card 41":12, "card 42":12, "card 45":13, "card 46":13, "card 49":14, "card 50":14]
+
+var challengerStartDeck = ["card 3":2, "card 4":2, "card 7":3, "card 8":3, "card 11":4, "card 12":4, "card 15":5, "card 16":5, "card 19":6, "card 20":6,  "card 23":7, "card 24":7, "card 27":8, "card 28":8, "card 31":9, "card 32":9, "card 35":10, "card 36":10, "card 39":11, "card 40":11, "card 43":12, "card 44":12, "card 47":13, "card 48":13, "card 51":14, "card 52":14]
